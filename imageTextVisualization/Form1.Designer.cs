210a1b1fb530dc5a48ddecf597a3b5348672871c
DESKTOP-3F31F4F\Nitipol Pattanapun <icenitipol@gmail.com>
2021-01-18 11:41:03 +0700

    first commit

---
+++ imageTextVisualization/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿
+namespace imageTextVisualization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.translate = new System.Windows.Forms.TextBox();
+            this.back = new System.Windows.Forms.Button();
+            this.index = new System.Windows.Forms.TextBox();
+            this.next = new System.Windows.Forms.Button();
+            this.sel_img = new System.Windows.Forms.Button();
+            this.sel_txt = new System.Windows.Forms.Button();
+            this.tb_sel_img = new System.Windows.Forms.TextBox();
+            this.tb_sel_txt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.save = new System.Windows.Forms.Button();
+            this.original = new System.Windows.Forms.TextBox();
+            this.index_label = new System.Windows.Forms.Label();
+            this.copy_original = new System.Windows.Forms.Button();
+            this.paste_translate = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(568, 387);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightGreen;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(586, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(532, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ข้อความเดิม";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.LightGreen;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(585, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(532, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ข้อความแปล";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // translate
+            // 
+            this.translate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.translate.Location = new System.Drawing.Point(586, 193);
+            this.translate.Multiline = true;
+            this.translate.Name = "translate";
+            this.translate.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.translate.Size = new System.Drawing.Size(469, 63);
+            this.translate.TabIndex = 4;
+            this.translate.Text = "a test text for shsdgdsgsdgdsgsdgdsgdgdsgs gsdgsdgsd gdssgsdg gdsgsdgsow adfhdfhd" +
+    "fhdfhdfhdffhdfhdha aha test test blabla hello world hahasafasa safasfsaf afsf";
+            this.translate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // back
+            // 
+            this.back.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.back.Location = new System.Drawing.Point(586, 316);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(191, 39);
+            this.back.TabIndex = 7;
+            this.back.Text = "ย้อนกลับ";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // index
+            // 
+            this.index.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.index.Location = new System.Drawing.Point(783, 316);
+            this.index.Name = "index";
+            this.index.Size = new System.Drawing.Size(138, 26);
+            this.index.TabIndex = 9;
+            this.index.Text = "99999";
+            this.index.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.index.KeyDown += new System.Windows.Forms.KeyEventHandler(this.index_KeyDown);
+            // 
+            // next
+            // 
+            this.next.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.next.Location = new System.Drawing.Point(927, 316);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(191, 39);
+            this.next.TabIndex = 10;
+            this.next.Text = "ถัดไป";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // sel_img
+            // 
+            this.sel_img.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sel_img.Location = new System.Drawing.Point(586, 372);
+            this.sel_img.Name = "sel_img";
+            this.sel_img.Size = new System.Drawing.Size(121, 39);
+            this.sel_img.TabIndex = 11;
+            this.sel_img.Text = "เลือก";
+            this.sel_img.UseVisualStyleBackColor = true;
+            this.sel_img.Click += new System.EventHandler(this.sel_img_Click);
+            // 
+            // sel_txt
+            // 
+            this.sel_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sel_txt.Location = new System.Drawing.Point(586, 417);
+            this.sel_txt.Name = "sel_txt";
+            this.sel_txt.Size = new System.Drawing.Size(121, 39);
+            this.sel_txt.TabIndex = 12;
+            this.sel_txt.Text = "เลือก";
+            this.sel_txt.UseVisualStyleBackColor = true;
+            this.sel_txt.Click += new System.EventHandler(this.sel_txt_Click);
+            // 
+            // tb_sel_img
+            // 
+            this.tb_sel_img.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_sel_img.Location = new System.Drawing.Point(713, 372);
+            this.tb_sel_img.Multiline = true;
+            this.tb_sel_img.Name = "tb_sel_img";
+            this.tb_sel_img.ReadOnly = true;
+            this.tb_sel_img.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb_sel_img.Size = new System.Drawing.Size(405, 39);
+            this.tb_sel_img.TabIndex = 13;
+            this.tb_sel_img.Text = "เลือก Folder ที่เก็บรูป";
+            this.tb_sel_img.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb_sel_txt
+            // 
+            this.tb_sel_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_sel_txt.Location = new System.Drawing.Point(713, 417);
+            this.tb_sel_txt.Multiline = true;
+            this.tb_sel_txt.Name = "tb_sel_txt";
+            this.tb_sel_txt.ReadOnly = true;
+            this.tb_sel_txt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb_sel_txt.Size = new System.Drawing.Size(405, 39);
+            this.tb_sel_txt.TabIndex = 14;
+            this.tb_sel_txt.Text = "เลือกไฟล์ List ข้อความ";
+            this.tb_sel_txt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Salmon;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1106, 45);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "แปลภาษา V0.01";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // save
+            // 
+            this.save.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save.Location = new System.Drawing.Point(586, 271);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(532, 39);
+            this.save.TabIndex = 16;
+            this.save.Text = "บันทึก";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // original
+            // 
+            this.original.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.original.Location = new System.Drawing.Point(586, 95);
+            this.original.Multiline = true;
+            this.original.Name = "original";
+            this.original.ReadOnly = true;
+            this.original.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.original.Size = new System.Drawing.Size(469, 63);
+            this.original.TabIndex = 17;
+            this.original.Text = "a test text for shsdgdsgsdgdsgsdgdsgdgdsgs gsdgsdgsd gdssgsdg gdsgsdgsow adfhdfhd" +
+    "fhdfhdfhdffhdfhdha aha test test blabla hello world hahasafasa safasfsaf afsf";
+            this.original.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // index_label
+            // 
+            this.index_label.Location = new System.Drawing.Point(783, 345);
+            this.index_label.Name = "index_label";
+            this.index_label.Size = new System.Drawing.Size(138, 22);
+            this.index_label.TabIndex = 18;
+            this.index_label.Text = "Total : ???";
+            this.index_label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // copy_original
+            // 
+            this.copy_original.Location = new System.Drawing.Point(1063, 96);
+            this.copy_original.Name = "copy_original";
+            this.copy_original.Size = new System.Drawing.Size(55, 62);
+            this.copy_original.TabIndex = 19;
+            this.copy_original.Text = "COPY";
+            this.copy_original.UseVisualStyleBackColor = true;
+            this.copy_original.Click += new System.EventHandler(this.copy_original_Click);
+            // 
+            // paste_translate
+            // 
+            this.paste_translate.Location = new System.Drawing.Point(1063, 194);
+            this.paste_translate.Name = "paste_translate";
+            this.paste_translate.Size = new System.Drawing.Size(55, 62);
+            this.paste_translate.TabIndex = 20;
+            this.paste_translate.Text = "PASTE";
+            this.paste_translate.UseVisualStyleBackColor = true;
+            this.paste_translate.Click += new System.EventHandler(this.paste_translate_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 462);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1104, 409);
+            this.webBrowser1.TabIndex = 21;
+            this.webBrowser1.Url = new System.Uri("https://translate.google.com/?sl=en&tl=th&op=translate&hl=th", System.UriKind.Absolute);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 883);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.paste_translate);
+            this.Controls.Add(this.copy_original);
+            this.Controls.Add(this.index_label);
+            this.Controls.Add(this.original);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_sel_txt);
+            this.Controls.Add(this.tb_sel_img);
+            this.Controls.Add(this.sel_txt);
+            this.Controls.Add(this.sel_img);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.index);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.translate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "แปลภาษา V0.01";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox translate;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.TextBox index;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.Button sel_img;
+        private System.Windows.Forms.Button sel_txt;
+        private System.Windows.Forms.TextBox tb_sel_img;
+        private System.Windows.Forms.TextBox tb_sel_txt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.TextBox original;
+        private System.Windows.Forms.Label index_label;
+        private System.Windows.Forms.Button copy_original;
+        private System.Windows.Forms.Button paste_translate;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ imageTextVisualization/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace imageTextVisualization
+{
+    public partial class Form1 : Form
+    {
+
+        //public String[] path;
+        public String[] originalLines;
+        public String[] translateLines;
+
+        public String imgPath = "";
+        public String txtPath = "";
+        public String outputTxtPath = "";
+
+        public int len = 0;
+        public int currentIndex = 0;
+
+
+        public void setEnableWorkspace(bool val)
+        {
+            save.Enabled = val;
+            next.Enabled = val;
+            back.Enabled = val;
+
+            original.Enabled = val;
+            translate.Enabled = val;
+            index.Enabled = val;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            setEnableWorkspace(false);
+        }
+
+        private void sel_img_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    imgPath = fbd.SelectedPath;
+                    tb_sel_img.Text = imgPath;
+                    loadFile();
+                }
+            }
+        }
+
+        private void sel_txt_Click(object sender, EventArgs e)
+        {
+            using (var file = new OpenFileDialog())
+            {
+                DialogResult result = file.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(file.FileName))
+                {
+                    txtPath = file.FileName;
+                    tb_sel_txt.Text = txtPath;
+                    loadFile();
+                }
+            }
+        }
+
+        public void errorDialog(String txt)
+        {
+            DialogResult res = MessageBox.Show(txt, "โปรดตรวจสอบ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void loadFile()
+        {
+            if (!string.IsNullOrWhiteSpace(txtPath) && !string.IsNullOrWhiteSpace(imgPath))
+            {
+
+                try
+                {
+
+                    //String fileName = txtPath.Replace(".txt","_output.txt");
+
+
+                    originalLines = System.IO.File.ReadAllLines(txtPath);
+                    originalLines = originalLines.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+                    len = originalLines.Length;
+
+
+                    //Console.WriteLine("trans line total = " + translateLines.Length);
+
+                    Console.WriteLine("text length = " + len);
+                    index_label.Text = "Total : " + len;
+
+                    if(File.Exists(txtPath.Replace(".txt", "_output.txt")))
+                    {
+                        Console.WriteLine("FOUND SAVE");
+                        translateLines = System.IO.File.ReadAllLines(txtPath.Replace(".txt", "_output.txt"));
+
+                        if (translateLines.Length != originalLines.Length)
+                        {
+                            Console.WriteLine("length problem.. discard save");
+                            translateLines = new string[len];
+                        }
+                        else
+                        {
+                            try
+                            {
+                                for(int i = 0; i < translateLines.Length; i++)
+                                {
+                                    translateLines[i] = translateLines[i].Substring(translateLines[i].IndexOf(',') + 1);
+                                }
+                            }
+                            catch
+                            {
+                                Console.WriteLine("length problem.. discard save");
+                                translateLines = new string[len];
+                            }
+                        }
+                    }
+                    else
+                    {
+                        translateLines = new string[len];
+                    }
+
+                    //verify
+                    bool valid = originalLines[0].Split(',')[0].Split('/')[0] == ".." ? true : false;
+                    if (valid)
+                    {
+                        updateWorkspace(0);
+                        setEnableWorkspace(true);
+                    }
+                    else
+                    {
+                        setEnableWorkspace(false);
+                        errorDialog("ไฟล์อาจผิดพลาด โปรดตรวจสอบ");
+                    }
+ 
+                }
+                catch
+                {
+                    setEnableWorkspace(false);
+                    errorDialog("ไฟล์อาจผิดพลาด โปรดตรวจสอบ");
+                }
+            }
+        }
+
+        public void updateWorkspace(int position)
+        {
+            currentIndex = position;
+
+            index.Text = currentIndex+1 + "";
+            String currentLine = originalLines[currentIndex].Substring(originalLines[currentIndex].IndexOf(',') + 1);
+
+            original.Text = currentLine;
+            translate.Text = translateLines[currentIndex];
+            if ((String.IsNullOrEmpty(translateLines[currentIndex])?"": translateLines[currentIndex]).Equals(currentLine))
+            {
+                translate.Text = "";
+            }
+
+            String currentImage = imgPath+ "\\" + originalLines[currentIndex].Split(',')[0].Split('/')[originalLines[currentIndex].Split(',')[0].Split('/').Length - 1];
+            Console.WriteLine("File Image Path = " + currentImage);
+            pictureBox1.Image = Image.FromFile(currentImage);
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            save_Click(sender,e);
+
+            if (currentIndex > 0)
+            {
+                currentIndex -= 1;
+            }
+            else if (currentIndex == 0)
+            {
+                currentIndex = len-1;
+            }
+            updateWorkspace(currentIndex);
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            save_Click(sender, e);
+
+            if (currentIndex < len-1)
+            {
+                currentIndex += 1;
+            }
+            else if (currentIndex == len-1)
+            {
+                currentIndex = 0;
+            }
+            updateWorkspace(currentIndex);
+        }
+
+        private void copy_original_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(original.Text);
+        }
+
+        private void paste_translate_Click(object sender, EventArgs e)
+        {
+            translate.Text = Clipboard.GetText();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            translateLines[currentIndex] = translate.Text;
+
+            saveFile();
+        }
+
+        public void saveFile()
+        {
+            String fileName = txtPath.Replace(".txt","_output.txt");
+            String fileName_backup = "c:/backup_translate.txt";
+            try
+            {
+                // Check if file already exists. If yes, delete it.     
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+
+                // Create a new file     
+                using (FileStream fs = File.Create(fileName))
+                {
+                    // Add some text to file    
+                    //fs.Write(translateLines);
+
+                    String output = "";
+
+                    for (int i = 0; i < translateLines.Length; i++)
+                    {
+                        if(!String.IsNullOrEmpty(translateLines[i]))
+                        output += originalLines[i].Split(',')[0] + "," + translateLines[i].Trim() + "\n";
+                        else
+                         output += originalLines[i].Split(',')[0] + "," + originalLines[i].Substring(originalLines[i].IndexOf(',')+1) + "\n";
+
+                    }
+
+                    output = output.Substring(0, output.Length - 2);
+
+                    byte[] bytes = Encoding.UTF8.GetBytes(output);
+
+                    fs.Write(bytes, 0, bytes.Length);
+                }
+
+            }
+            catch
+            {
+                errorDialog("ไม่สามารถบันทึกไฟล์ โปรดตรวจสอบ...");
+                errorDialog("ระบบจะสร้าง Backup ไว้ที่ c:/backup_translate.txt");
+
+                try
+                {
+                    // Check if file already exists. If yes, delete it.     
+                    if (File.Exists(fileName_backup))
+                    {
+                        File.Delete(fileName_backup);
+                    }
+
+                    // Create a new file     
+                    using (FileStream fs = File.Create(fileName_backup))
+                    {
+                        // Add some text to file    
+                        //fs.Write(translateLines);
+
+                        String output = "";
+
+                        for(int i = 0; i < translateLines.Length; i++)
+                        {
+                            output += originalLines[i].Split(',')[0] + "," + translateLines[i] + "\n";
+                        }
+
+                        byte[] bytes = Encoding.UTF8.GetBytes(output);
+
+                        fs.Write(bytes,0,0);
+                    }
+
+                }
+                catch
+                {
+                    errorDialog("ไม่สามารถบันทึกไฟล์ โปรดตรวจสอบ...");
+                }
+            }
+        }
+
+        private void index_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Console.WriteLine("enter");
+
+                int index_get = 0;
+
+                if (int.TryParse(index.Text, out index_get))
+                {
+                    if(index_get>=1 && index_get <= len)
+                    {
+
+                        currentIndex = index_get - 1;
+
+                        updateWorkspace(currentIndex);
+
+                    }
+                    else
+                    {
+                        index.Text = (currentIndex + 1).ToString();
+                    }
+                }
+                else
+                {
+                    index.Text = (currentIndex + 1).ToString();
+                }
+            }
+        }
+    }
+}
